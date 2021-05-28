3e26f42814a1a8ad67cd55d95b4d5e662d4b08e3
xoo0ch <xochvilla15@gmail.com>
2021-05-28 11:17:25 -0500

    :recycle: refactorizacion de App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import './App.css';
 import CarouselContainer from './Component/CarouselContainer';
+import Fca from './Component/Fca';
 import React, { useState } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 
 function App() {
-    const [dropdown, setDropdown] = useState(false);
-  
-    const abrirCerrarDropdown=()=>{
-      setDropdown(!dropdown);
-    }
-    const accionPrueba=()=>{
-      alert(<a  href="http://www.fca.uaemex.mx/">Página Oficial</a> );
-    }
-  return (
-    <div className="App">
-      
-      <CarouselContainer/>
-      <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown} direction="up" >
-      <DropdownToggle caret className="botonDropdown"> 
-     
-        INFORMATE MÁS...
+  const [dropdown, setDropdown] = useState(false);
+
+  const abrirCerrarDropdown = () => {
+    setDropdown(!dropdown);
+  }
+  // const accionPrueba = () => {
+  //   alert(<a href="http://www.fca.uaemex.mx/">Página Oficial</a>);
+  // }
+
+  const boton = (
+    <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown} direction="up" >
+            <DropdownToggle caret className="botonDropdown">
+
+              INFORMATE MÁS...
       </DropdownToggle>
 
-      <DropdownMenu>
-        <DropdownItem onClick={()=>accionPrueba()}><center>FAC</center></DropdownItem> <center> <a  href="http://www.fca.uaemex.mx/">Página Oficial</a> </center>
-        <DropdownItem divider/>
-        <DropdownItem><center>BECAS Y SERVICIO SOCIAL</center></DropdownItem>
-        <DropdownItem divider/>
-        <DropdownItem><center>INFORMES Y REDES SOCIALES</center></DropdownItem>
-        
-      </DropdownMenu>
-    </Dropdown>
+            <DropdownMenu>
+              <DropdownItem ><center>FAC</center></DropdownItem> <center> <Link to={"/fca"}>Página Oficial</Link></center>
+              <DropdownItem divider />
+              <DropdownItem><center>SERVICIO SOCIAL</center></DropdownItem> <center> <a href="http://sev.uaemex.mx/index.php/servicios/servicio-social">Servicio Social</a> </center>
+              <DropdownItem divider />
+              <DropdownItem><center>BECAS</center></DropdownItem> <center><a href="https://sibecas.uaemex.mx/">Becas</a></center>
+              <DropdownItem divider />
+              <DropdownItem><center>CONTACTO Y REDES SOCIALES</center></DropdownItem> <center> <a href="https://www.uaemex.mx/mi-universidad/directorio-telef%C3%B3nico.html">Contacto</a> </center>
 
-    </div>
+            </DropdownMenu>
+          </Dropdown>
+  )
+  return (
+    <Router>
+      <Switch>
+        <div className="App">
+
+          <Route exact path="/" component={CarouselContainer} />
+          <Route path="/fca" component={Fca} />
+          
+          { boton }
+        </div>
+      </Switch>
+    </Router>
   );
 }
 

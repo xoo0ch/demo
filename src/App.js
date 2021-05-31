25d1ee43432c922bdf085e662627d073f02476ce
xoo0ch <xochvilla15@gmail.com>
2021-05-31 12:59:17 -0500

    :recycle: refactor app

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Fca from './Component/Fca';
 import React, { useState } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-function App() {
-const [dropdown, setDropdown] = useState(false);
+
+export default function App() {
+
+  const [dropdown, setDropdown] = useState(false);
 
   const abrirCerrarDropdown = () => {
     setDropdown(!dropdown);
   }
- 
   const boton = (
     <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown} direction="up" >
-            <DropdownToggle caret className="botonDropdown">
+      <DropdownToggle caret className="botonDropdown">
 
-              INFORMATE MÁS...
-      </DropdownToggle>
+
+        INFORMATE MÁS...
+              </DropdownToggle>
       <DropdownMenu>
-              <DropdownItem ><center>FAC</center></DropdownItem> <center> <Link to={"/fca"}>Página Oficial</Link></center>
-              <DropdownItem divider />
-              <DropdownItem><center>SERVICIO SOCIAL</center></DropdownItem> <center> <a href="http://sev.uaemex.mx/index.php/servicios/servicio-social">Servicio Social</a> </center>
-              <DropdownItem divider />
-              <DropdownItem><center>BECAS</center></DropdownItem> <center><a href="https://sibecas.uaemex.mx/">Becas</a></center>
-              <DropdownItem divider />
-              <DropdownItem><center>CONTACTO Y REDES SOCIALES</center></DropdownItem> <center> <a href="https://www.uaemex.mx/mi-universidad/directorio-telef%C3%B3nico.html">Contacto</a> </center>
-
-            </DropdownMenu>
-          </Dropdown>
+        <DropdownItem ><center>FAC</center></DropdownItem> <center> <Link to={"/fca"}>Página Oficial</Link></center>
+        <DropdownItem divider />
+        <DropdownItem><center>SERVICIO SOCIAL</center></DropdownItem> <center> <a href="http://sev.uaemex.mx/index.php/servicios/servicio-social">Servicio Social</a> </center>
+        <DropdownItem divider />
+        <DropdownItem><center>BECAS</center></DropdownItem> <center><a href="https://sibecas.uaemex.mx/">Becas</a></center>
+        <DropdownItem divider />
+        <DropdownItem><center>CONTACTO Y REDES SOCIALES</center></DropdownItem> <center> <a href="https://www.uaemex.mx/mi-universidad/directorio-telef%C3%B3nico.html">Contacto</a> </center>
+      </DropdownMenu>
+    </Dropdown>
   )
   return (
     <Router>
@@ -36,11 +37,10 @@ const [dropdown, setDropdown] = useState(false);
 
           <Route exact path="/" component={CarouselContainer} />
           <Route path="/fca" component={Fca} />
-          
-          { boton }
+
+          {boton}
         </div>
       </Switch>
     </Router>
   );
-  }
-export default App;
+}
